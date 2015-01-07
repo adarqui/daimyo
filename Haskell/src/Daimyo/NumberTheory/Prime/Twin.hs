71125147f47948b69f81afec74487a3ea0a5ae2c
Andrew Darqui <andrew.darqui@gmail.com>
2015-01-07 01:29:28 -0500

    other than 4, even twins all disivible by 6?

--- Haskell/src/Daimyo/NumberTheory/Prime/Twin.hs
+++ Haskell/src/Daimyo/NumberTheory/Prime/Twin.hs
@@ -1,9 +1,13 @@
 module Daimyo.NumberTheory.Prime.Twin (
     twins,
-    twins'even
+    twins'even,
+    twins'even'divisible'by'6
 ) where
 
 import Daimyo.NumberTheory.Prime
+
+import Data.List
+
 
 {-
     twins: returns pairs of twin primes [(3,5),(5,7)...]
@@ -24,3 +28,7 @@ twins'even = map (\(x,_) -> x+1) twins
     Brun Constant
     Twin Prime Theorem
 -}
+
+
+-- everything divisible by 6? except the first one, 4, hence the tail
+twins'even'divisible'by'6 n = all (\k -> k `mod` 6 == 0) $ tail $ take n twins'even
