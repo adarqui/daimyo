ee0c8ab3096bf5a51081c3eede05165d6869581f
Andrew Darqui <andrew.darqui@gmail.com>
2015-01-11 01:56:00 -0500

    bernoulli algorithm

--- Haskell/src/Daimyo/NumberTheory/Bernoulli.hs
+++ Haskell/src/Daimyo/NumberTheory/Bernoulli.hs
@@ -1,8 +1,17 @@
 module Daimyo.NumberTheory.Bernoulli (
     bernoullis,
-    bernoulli
+    bernoulli,
+    k,
+    d,
+    n,
+    z,
+    a,
+    bm,
+    bm'
 ) where
 
+import Daimyo.Algebra.Divisibility
+import Daimyo.NumberTheory.Prime
 import Daimyo.NumberTheory.Factorial
 import Daimyo.NumberTheory.Constants.E
 
@@ -15,8 +24,55 @@ bernoulli x = x / ((e**x) - 1)
     implement this: http://wstein.org/projects/168/kevin_mcgown/bernproj.pdf
 -}
 
+k :: Double -> Double
 k m = (2*fac m)/((2*pi)**m)
 
---d = product p
+
+d :: Double -> Double
+d m =
+    let
+        divs = divisors 50
+        divsp1 = map succ divs
+        p = filter isPrime divsp1
+    in
+        fromIntegral (product p) :: Double
 
 
+n :: Double -> Double -> Double -> Double
+n m' k' d' =
+    let
+        v = ceiling $ (k'*d')**(1/(m'-1))
+    in
+        fromIntegral v :: Double
+
+
+z :: Double -> Double -> Double
+z m n' =
+    let
+        p = takeWhile (<=n') $ map (\p' -> fromIntegral p' :: Double) primes
+        v = map (\p' -> (1 - p'**(-m))**(-1)) p
+    in
+        product v
+
+
+a :: Double -> Double -> Double -> Double -> Double
+a m d' k' z' = ((-1)**(m/2+1))*(fromIntegral (ceiling(d'*k'*z')) :: Double)
+
+      
+bm 0 = 1
+bm 1 = -(1/2)
+bm m =
+    if (m >= 3 && odd (floor m))
+        then 0
+        else fst $ bm' m
+        
+
+bm' m =
+    let
+        k' = k m
+        d' = d m
+        n' = n m k' d'
+        z' = z m n'
+        a' = a m k' d' z'
+    in
+        (a' / d',(k',d',n',z',a'))
