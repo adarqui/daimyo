904313f891e52aca07e78ffba36a7a38a0dc904b
Dante Elrik <dante@cloudshopper.com>
2015-01-11 05:54:14 -0500

    Palindrom done

--- Haskell/src/Daimyo/Delrik/p.euler/4.hs
+++ Haskell/src/Daimyo/Delrik/p.euler/4.hs
@@ -9,9 +9,31 @@ where
 -- Find the largest palindrome made from the product of two 3-digit numbers.
 --
 -- poc:
--- filter (((==) 0 ) .( `mod` 2) . length . snd) $ map (\x -> ((length . show) x, show x)) $ zipWith (*) [100..999] [100..999]
+-- filter (((==) 0 ) . ( `mod` 2) . length . snd) $ map (\x -> ((length . show) x, show x)) $ zipWith (*) [100..999] [100..999]
 palindromes :: [Int]
 palindromes = 
-    [x*y | x <- [100..999] , y <- [100..999]]
+    map (read . snd) $ filter (\(ln,pdc) -> isPalindrome pdc) $ filter toEvenLengthPalindromes $ map vectorize $ [x * y | x <- [100..999], y <- [100..999]]
+    where
+        toEvenLengthPalindromes :: (Int,String) -> Bool
+        toEvenLengthPalindromes = ( ((==) 0 ) . ( `mod` 2) . length . snd)
 
-    foldl product 
+        vectorize :: Int -> (Int, String)
+        vectorize x = 
+            let 
+                sx = show x 
+            in (length sx, sx)
+-- abc xyz
+-- 900 009
+--
+isPalindrome :: String -> Bool
+isPalindrome (a:b:c:x:y:z:[])
+    | reverse [a,b,c] == [x,y,z]  = True
+    | otherwise =  False
+--isPalindrome' :: Int -> Bool
+--isPalindrome' i = x ++ y == i
+--    where
+--        x 
+--        width
+
+largestPalindrome :: Int
+largestPalindrome = (head . reverse) palindromes
