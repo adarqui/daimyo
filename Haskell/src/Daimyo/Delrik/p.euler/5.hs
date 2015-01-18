56b495aa23faf04667c6a4508e66982f2680e633
Dante Elrik <dante@cloudshopper.com>
2015-01-18 01:12:16 -0500

    Quick wip

--- Haskell/src/Daimyo/Delrik/p.euler/5.hs
+++ Haskell/src/Daimyo/Delrik/p.euler/5.hs
@@ -10,13 +10,6 @@ import Data.List
 -- http://owp.ghost.io/project-euler-1/
 --
 --
-isPrime :: (Enum a, Ord a, Integral a) => a -> Bool
-isPrime 1 = True
-isPrime 2 = True
-isPrime n = null [x | x <- possibles, n `mod` x == 0]
-    where
-        possibles = takeWhile (\x -> x*x <= n) [2..n]
-primes = 1 : 2 : filter isPrime [3..]
 
 euler'p2 :: (Floating a, Integral a) => a -> [(a,a)]
 euler'p2 x = takeWhile isPrime $ zip [x,(x-1)..hf] [1..x]
@@ -26,16 +19,30 @@ euler'p2 x = takeWhile isPrime $ zip [x,(x-1)..hf] [1..x]
         isPrime (x',y') = case (op y') of 
             0 -> False;
             _ -> True
-pf :: Integer-> [Integer] -> IO([Integer])
-pf 2 _= do return []
-pf 1 _= do return []
-pf n cc= putStrLn (show n ++ "," ++  show nf ++ "," ++  show sprimes) >> pf n' cc
+
+isPrime :: Float -> Bool
+isPrime 1 = True
+isPrime 2 = True
+isPrime n = null [x | x <- possibles, (round  n) `mod` (round x) == 0]
+    where
+        possibles :: [Float]
+        possibles = takeWhile (\x -> x*x <= n) [2..n]
+
+primes :: [Float]
+primes = 1 : 2 : filter isPrime [3..]
+pf :: Float -> IO ([Float], Float)
+pf 2 = return ([1,2], 2)
+pf 1 = return ([1,1], 1)
+pf n = pf' ([n], 0)
     where 
-        n'          = n `div` nf
-        nf          = head $ filter isDivisible sprimes
-        sprimes     = takeWhile (<n) $ tail primes
-        isDivisible = (\x -> (n `mod` x) == 0 )
---{- Prime Factorisation -}
---pf :: i -> a -> [a]
---pf _ 2 = []
---pf i n = (head [x | x <- primes, (x `mod` n) == 0]) : []
+        pf' :: ([Float],Float) -> IO ([Float], Float)
+        pf' (2:xs ,b) = return ([] ++ xs,b)
+        pf' (1:xs ,b) = return ([] ++ xs,b)
+
+        pf' (hd:ns,b) = do
+            putStrLn ("Hello: " ++ (show hd) ++ "|" ++ show b ++ "==" ++ show smallestPrime )  
+            let n' = hd / smallestPrime in pf' (n':smallestPrime:ns,succ b)
+
+        isMultiple :: Float -> Float -> Bool
+        isMultiple m n'' = (round m) `mod` (round n) == 0
+        smallestPrime = head $ filter (isMultiple n) $ tail primes
