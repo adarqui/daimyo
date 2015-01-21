4bd21bec0383b69a2211443aafbedda32e299631
Dante Elrik <dante@cloudshopper.com>
2015-01-21 12:09:31 -0500

    Initial 01

---
+++ Haskell/src/Daimyo/Delrik/CIS194/01/Main.hs
@@ -0,0 +1,14 @@
+module Main 
+(
+toDigits,
+main
+)
+where
+
+toDigits,toDigitsRev :: Integer -> [Integer]
+
+toDigits = undefined
+toDigitsRev = undefined
+
+main :: IO ()
+main = putStrLn "Excercise 1"
