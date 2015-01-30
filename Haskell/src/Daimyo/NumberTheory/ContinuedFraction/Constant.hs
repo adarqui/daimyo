6f353adb8703701e6ccd02b2a606a40671267e7c
Andrew Darqui <andrew.darqui@gmail.com>
2015-01-29 19:53:05 -0500

    messing around with continued fractions

--- Haskell/src/Daimyo/NumberTheory/ContinuedFraction/Constant.hs
+++ Haskell/src/Daimyo/NumberTheory/ContinuedFraction/Constant.hs
@@ -1,7 +1,10 @@
 module Daimyo.NumberTheory.ContinuedFraction.Constant (
     _pi,
     _e,
-    _goldenRatio
+    _goldenRatio,
+    _goldenRatioN,
+    nRatioN,
+    ratiosN
 ) where
 
 import Daimyo.NumberTheory.ContinuedFraction
@@ -11,3 +14,12 @@ _pi = continuedFraction (3, [7,15,1,292,1,1,1,2,1,3,1])
 _e = continuedFraction (2, [1,2,1,1,4,1,1,6,1,1,8])
 
 _goldenRatio = continuedFraction (1, [1,1,1,1,1,1,1,1,1,1,1])
+
+_goldenRatioN n = continuedFraction (1, replicate n 1)
+
+{-
+    n' + 1 / (n' + (1 / ...) ...)
+-}
+nRatioN n' n = continuedFraction (n', replicate n n')
+
+ratiosN n = map (\n' -> continuedFraction (n', replicate n n')) [1..]
