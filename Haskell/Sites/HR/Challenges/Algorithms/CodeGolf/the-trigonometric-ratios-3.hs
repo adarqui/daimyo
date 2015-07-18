8c72fe94494bb6a4f319df35be134defedacba61
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-17 21:38:41 -0400

    smaller

--- Haskell/Sites/HR/Challenges/Algorithms/CodeGolf/the-trigonometric-ratios-3.hs
+++ Haskell/Sites/HR/Challenges/Algorithms/CodeGolf/the-trigonometric-ratios-3.hs
@@ -6,4 +6,4 @@ k r z x=z-q 0+q 2-q 4+q 6 where q n=x**(r+n)/f(r+n)
 r d=printf"%.3f"(d::Double)
 m=map
 p=putStrLn
-main=getLine>>getContents>>= \l->mapM_(\i->(p$r$s i)>>(p$r$c i))$m read$lines l
+main=getLine>>getContents>>=mapM_(\i->(p$r$s i)>>(p$r$c i)).m read.lines
