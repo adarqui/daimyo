3fe1482c4f93d79a0cabbf25877dcc69560b88a5
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-21 16:41:57 -0400

    cleanup divisibility/common

--- Haskell/src/Daimyo/Algebra/Divisibility/Common.hs
+++ Haskell/src/Daimyo/Algebra/Divisibility/Common.hs
@@ -1,8 +1,14 @@
 module Daimyo.Algebra.Divisibility.Common (
-    common'divisor
+    commonDivisor
 ) where
 
-common'divisor d a b =
-    (a `mod` d == 0 && b `mod` d == 0)
-
-t_common'divisor = common'divisor 2 10 12
+-- | commonDivisor: is d a common divisor of a and b?d
+--
+-- >>> commonDivisor (2 :: Int) 10 12
+-- True
+--
+-- >>> commonDivisor (5 :: Int) 10 12
+-- False
+--
+commonDivisor :: Integral a => a -> a -> a -> Bool
+commonDivisor d a b = a `mod` d == 0 && b `mod` d == 0
