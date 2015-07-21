1a3ff9b825908723475b9bcf803ab2d82efe7ae3
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-21 15:47:05 -0400

    cleanup abstract algebra polynomial

--- Haskell/src/Daimyo/AbstractAlgebra/Polynomial.hs
+++ Haskell/src/Daimyo/AbstractAlgebra/Polynomial.hs
@@ -1,36 +1,42 @@
 {-# LANGUAGE NPlusKPatterns #-}
 
-{-
-    sources:
-
-        http://en.wikipedia.org/wiki/Binomial_theorem
--}
+--  sources:
+--  http://en.wikipedia.org/wiki/Binomial_theorem
 
 module Daimyo.AbstractAlgebra.Polynomial (
-    fac,
-    binomial,
-    pascal'data,
-    t_pascal'data
+  fac,
+  binomial,
+  pascalData,
 ) where
 
-import Daimyo.Combinatorics.Combination
+import           Daimyo.Combinatorics.Combination
 
-fac 0 = 1
+-- | fac
+--
+-- >>> fac 5 :: Int
+-- 120
+--
+fac :: Integral a => a -> a
+fac 0     = 1
 fac (n+1) = (n+1) * fac n
 
-{-
-    (x + y)^n = sum from k=0 to n of(n `choose` k)x^(n-k)y^k
-    (x + y)^n = sum from k=0 to n of(n `choose` k)x^ky^(n-k)
--}
+--    (x + y)^n = sum from k=0 to n of(n `choose` k)x^(n-k)y^k
+--    (x + y)^n = sum from k=0 to n of(n `choose` k)x^ky^(n-k)
 
-binomial x y n =
-    let
-        formula k = (n `choose` k) * ((x^(n-k))*(y^k))
-    in
-        map formula [0..n]
+-- | binomial
+--
+-- >>> binomial 5 5 5 :: [Int]
+-- [3125,15625,31250,31250,15625,3125]
+--
+binomial :: Integral a => a -> a -> a -> [a]
+binomial x y n = map formula [0..n]
+  where
+    formula k = (n `choose` k) * (x^(n-k))*(y^k)
 
-pascal'data x y n = map (binomial x y) [1..n]
-
---pretty'pascal'data =
-
-t_pascal'data = pascal'data 1 1 10
+-- | pascalData
+--
+-- >>> pascalData 5 5 5 :: [[Int]]
+-- [[5,5],[25,50,25],[125,375,375,125],[625,2500,3750,2500,625],[3125,15625,31250,31250,15625,3125]]
+--
+pascalData :: Integral a => a -> a -> a -> [[a]]
+pascalData x y n = map (binomial x y) [1..n]
