0af3eb7a7ac59fa78bbd011f21ba74ec92e02720
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-22 03:15:19 -0400

    exponent cleanup

--- Haskell/src/Daimyo/Algebra/Exponent.hs
+++ Haskell/src/Daimyo/Algebra/Exponent.hs
@@ -1,14 +1,19 @@
 module Daimyo.Algebra.Exponent (
-    negPow
+  negPow
 ) where
 
-{-
-    for my bernoulli functions
--}
-
+-- | negPow: for my bernoulli functions apparently
+--
+-- >>> negPow 2 (-2) :: Double
+-- 0.25
+--
+negPow :: (RealFrac a, Floating a) => a -> a -> a
 negPow x n
-    | x < 0 =
-        if (odd (floor n))
-            then (-1)*((abs x)**n)
-            else ((abs x)**n)
-    | x >= 0 = x ** n
+  | x < 0     = firstCase
+  | otherwise = x ** n
+  where
+    absx = abs x
+    firstCase =
+      if (odd $ floor n)
+        then (-1)*(absx**n)
+        else absx**n
