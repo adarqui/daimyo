35143cd58fb52ef953f5e98897e86aac456dfe65
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-22 03:18:21 -0400

    expression cleanup, what was I going to do with this again? some kind of algebraic calculator

--- Haskell/src/Daimyo/Algebra/Expression.hs
+++ Haskell/src/Daimyo/Algebra/Expression.hs
@@ -1,17 +1,37 @@
 module Daimyo.Algebra.Expression (
+  Degree,
+  Coeff,
+  Var (..),
+  Value (..),
+  Expr (..)
 ) where
 
 type Degree = Int
-type Coeff = Int
+type Coeff  = Int
 
-data Var = X | Y | Z deriving (Show)
+data Var
+  = X
+  | Y
+  | Z
+  deriving (Show)
 
-data Value a = Const a | Frac a a deriving (Show)
+data Value a
+  = Const a
+  | Frac a a
+  deriving (Show)
 
-data Expr = ConstTerm Double Degree | VarTerm Coeff Var Degree | Add Expr Expr | Sub Expr Expr | Mul Expr Expr | Div Expr Expr | Sqrt Expr | Exp Expr Int deriving (Show)
+data Expr
+  = ConstTerm Double Degree
+  | VarTerm Coeff Var Degree
+  | Add Expr Expr
+  | Sub Expr Expr
+  | Mul Expr Expr
+  | Div Expr Expr
+  | Sqrt Expr
+  | Exp Expr Int deriving (Show)
 
 -- simplify :: Expr -> Expr
--- eval :: Expr -> 
+-- eval :: Expr ->
 
 t_expr1 = Add (ConstTerm 5 1) (ConstTerm 2 2)
 t_expr2 = Add (VarTerm 1 X 2) (VarTerm 3 X 4)
