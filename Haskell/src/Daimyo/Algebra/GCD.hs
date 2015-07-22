76bcd01df0ab89b0bf4527ef44f6ef6adb68c39e
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-22 03:30:52 -0400

    gcd cleanup

--- Haskell/src/Daimyo/Algebra/GCD.hs
+++ Haskell/src/Daimyo/Algebra/GCD.hs
@@ -1,10 +1,14 @@
 module Daimyo.Algebra.GCD (
     module Daimyo.NumberTheory.GCD,
-    euclid,
-    euclid'list
+    euclid
 ) where
 
-import Daimyo.NumberTheory.GCD
+import           Daimyo.NumberTheory.GCD
 
+-- | euclid
+--
+-- >>> euclid 27 18
+-- 9
+--
+euclid :: Integer -> Integer -> Integer
 euclid = gcd
-euclid'list = [0]
