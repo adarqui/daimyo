276c60be6e5f91952018afe3a44b47eb67b67efb
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-23 14:15:00 -0400

    computing the gcd hrank

---
+++ Haskell/Sites/HR/Challenges/FunctionalProgramming/Recursion/computing-the-gcd.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Control.Applicative
+import Text.Printf
+
+main :: IO ()
+main = do
+  [x, y] :: [Int] <- (map read . words) <$> getLine
+  putStrLn $ show [x, y]
+
+-- | myGcd
+--
+-- >>> myGcd 10 100
+-- 10
+--
+myGcd :: Integral a => a -> a -> a
+myGcd a b
+      | a == b = a
+      | a > b  = gcd (a-b) b
+      | b > a  = gcd a (b-a)
+
+-- | format
+--
+-- >>> format 10 100 10
+-- "GCD(10,100) = 10"
+--
+format :: Int -> Int -> Int -> String
+format = printf "GCD(%d,%d) = %d"
