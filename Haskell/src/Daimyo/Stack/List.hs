901275c472db15d229fe85a36ada3fd56c335e68
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-23 00:40:31 -0400

    naive stack using a list

---
+++ Haskell/src/Daimyo/Stack/List.hs
@@ -0,0 +1,42 @@
+module Daimyo.Stack.List (
+  Stack,
+  push,
+  pop,
+  top,
+  empty,
+  isEmpty
+) where
+
+-- | Stack
+--
+newtype Stack a
+  = Stack [a]
+  deriving (Show)
+
+-- | push
+--
+push :: a -> Stack a -> Stack a
+push x (Stack xs) = Stack (x:xs)
+
+-- | pop
+--
+pop :: Stack a -> Maybe (Stack a)
+pop (Stack [])     = Nothing
+pop (Stack (_:xs)) = Just $ Stack xs
+
+-- | top
+--
+top :: Stack a -> Maybe a
+top (Stack [])    = Nothing
+top (Stack (x:_)) = Just x
+
+-- | empty
+--
+empty :: Stack a
+empty = Stack []
+
+-- | isEmpty
+--
+isEmpty :: Stack a -> Bool
+isEmpty (Stack []) = True
+isEmpty _          = False
