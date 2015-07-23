bcb0c5ab5229a19915be7e2df3675a0bd93bbb00
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-22 20:41:23 -0400

    permutation cycle cleanup

--- Haskell/src/Daimyo/Algebra/Permutation/Cycle.hs
+++ Haskell/src/Daimyo/Algebra/Permutation/Cycle.hs
@@ -1,20 +1,34 @@
 module Daimyo.Algebra.Permutation.Cycle (
-    kcycle,
-    cyclek,
-    cycle2,
-    cycle3
+ kcycle,
+  cyclek,
+  cycle2,
+  cycle3
 ) where
 
-import Data.List
+import           Data.List
 
-kcycle k s =
-    let
-        a = take k s
-        b = drop k s
-    in
-        map (++ b) $ permutations a
+-- | kcycle
+--
+-- >>> kcycle 2 ([1,2,3,4] :: [Int])
+-- [[1,2,3,4],[2,1,3,4]]
+--
+kcycle :: Int -> [a] -> [[a]]
+kcycle k s = map (++ b) $ permutations a
+  where
+    a = take k s
+    b = drop k s
 
+-- | cyclek
+--
+cyclek :: Int -> [a] -> [[a]]
 cyclek = kcycle
 
+-- | cycle2
+--
+cycle2 :: [a] -> [[a]]
 cycle2 = kcycle 2
+
+-- | cycle3
+--
+cycle3 :: [a] -> [[a]]
 cycle3 = kcycle 3
