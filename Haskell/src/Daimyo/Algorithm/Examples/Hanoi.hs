810947d72889981284c672d1dbc55d86e803db5a
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-22 20:48:49 -0400

    hanoi cleanup

--- Haskell/src/Daimyo/Algorithm/Examples/Hanoi.hs
+++ Haskell/src/Daimyo/Algorithm/Examples/Hanoi.hs
@@ -1,14 +1,16 @@
 module Daimyo.Algorithm.Examples.Hanoi (
-    hanoi
+  hanoi
 ) where
 
-hanoi 1 fromPeg toPeg = [(1, fromPeg, toPeg)] 
-hanoi n fromPeg toPeg =
-    let
-        unusedPeg = 6 - fromPeg - toPeg
-        a = hanoi (n-1) fromPeg unusedPeg
-        b = hanoi (n-1) unusedPeg toPeg
-    in
-        a ++ [(n, fromPeg, toPeg)] ++ b
-
-t_hani = hanoi 4 1 3
+-- | hanoi
+--
+-- >>> hanoi 4 1 3 :: [(Int,Int,Int)]
+-- [(1,1,2),(2,1,3),(1,2,3),(3,1,2),(1,3,1),(2,3,2),(1,1,2),(4,1,3),(1,2,3),(2,2,1),(1,3,1),(3,2,3),(1,1,2),(2,1,3),(1,2,3)]
+--
+hanoi :: (Num a, Num t, Eq t) => t -> a -> a -> [(t, a, a)]
+hanoi 1 fromPeg toPeg = [(1, fromPeg, toPeg)]
+hanoi n fromPeg toPeg = a ++ [(n, fromPeg, toPeg)] ++ b
+  where
+    unusedPeg = 6 - fromPeg - toPeg
+    a         = hanoi (n-1) fromPeg unusedPeg
+    b         = hanoi (n-1) unusedPeg toPeg
