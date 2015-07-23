80da7a3b187ef7639924375a9aaff5300965611b
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-23 00:49:49 -0400

    naive queue

---
+++ Haskell/src/Daimyo/Queue/Naive.hs
@@ -0,0 +1,60 @@
+module Daimyo.Queue.Naive (
+  Queue,
+  empty,
+  size,
+  enqueue,
+  dequeue,
+  bottom,
+  fromList,
+  toList
+) where
+
+-- | Queue
+--
+data Queue a
+  = Empty
+  | Node a (Queue a)
+  deriving (Show, Eq)
+
+-- | empty
+--
+empty :: Queue a
+empty = Empty
+
+-- | size
+--
+size :: (Num b) => Queue a -> b
+size Empty      = 0
+size (Node _ r) = 1 + size r
+
+-- | enqueue
+--
+enqueue :: a -> Queue a -> Queue a
+enqueue a Empty        = Node a Empty
+enqueue a (Node a' r') = Node a' (enqueue a r')
+
+-- | dequeue
+--
+dequeue :: Queue a -> Maybe (Queue a)
+dequeue Empty      = Nothing
+dequeue (Node a r) = Just r
+
+-- | bottom
+--
+bottom :: Queue a -> Maybe a
+bottom Empty      = Nothing
+bottom (Node a _) = Just a
+
+-- | fromList
+--
+fromList :: [a] -> Queue a
+fromList = go Empty
+  where
+    go q []     = q
+    go q (x:xs) = go (enqueue x q) xs
+
+-- | toList
+--
+toList :: Queue a -> [a]
+toList Empty      = []
+toList (Node a r) = a : toList r
