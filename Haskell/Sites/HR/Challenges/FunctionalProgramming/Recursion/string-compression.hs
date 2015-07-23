825722f8a0121c30671cee6c5c79aaadd18bdaf5
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-23 12:58:09 -0400

    string-compression cleanup

--- Haskell/Sites/HR/Challenges/FunctionalProgramming/Recursion/string-compression.hs
+++ Haskell/Sites/HR/Challenges/FunctionalProgramming/Recursion/string-compression.hs
@@ -1,23 +1,35 @@
 import           Data.Char
 import           Data.List
 
+-- | main
+--
 main :: IO ()
 main = do
- msg <- getLine
- let msg'len = length msg
- case (all isLower msg && (msg'len >= 1 && msg'len <= (10^5))) of
-  True -> putStrLn $ concat $ map compress $ frequency msg
-  _ -> error "Constraint Error: 1 <= length(msg) <= 10^5 && msg consists of lowercase Latin characters ('a'-'z') only."
- return ()
+  msg <- getLine
+  let msg_length = length msg
+  if (all isLower msg && (msg_length >= 1 && msg_length <= (10^5)))
+     then putStrLn $ concat $ map compress $ frequency msg
+     else error "Constraint Error: 1 <= length(msg) <= 10^5 && msg consists of lowercase Latin characters ('a'-'z') only."
 
+-- | compress
+--
+-- >>> compress (5, 'a')
+-- "a5"
+--
+compress :: (Show a, Ord a, Num a) => (a, Char) -> [Char]
 compress (n, c) =
- case (n <= 1) of
-  True -> [c]
-  _ -> [c] ++ show n
+  if n <= 1
+     then [c]
+     else [c] ++ show n
 
+-- | frequency
+--
+-- >>> frequency "abbcccdddd"
+-- [(1,'a'),(2,'b'),(3,'c'),(4,'d')]
+--
 frequency :: (Eq a) => [a] -> [(Int,a)]
 frequency [] = []
-frequency (x:xs) = (half'fst, x) : (frequency half'snd)
- where
-  half'fst = succ $ length $ takeWhile (== x) xs
-  half'snd = dropWhile (== x) xs
+frequency (x:xs) = (l, x) : (frequency r)
+  where
+    l = succ $ length $ takeWhile (== x) xs
+    r = dropWhile (== x) xs
