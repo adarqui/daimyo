d8d1ab57b63cf53d6a5ff0e70d386fc2d3e87141
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-24 18:49:53 -0400

    heap sort

--- Haskell/src/Daimyo/Sorting/HeapSort/Tree.hs
+++ Haskell/src/Daimyo/Sorting/HeapSort/Tree.hs
@@ -6,6 +6,9 @@ import           Daimyo.Queue.PriorityQueue.LeftistHeap
 
 -- | hsort
 --
+-- >>> hsort [1,3,1,9,4,2,3,0,4,5,10,0,0,7,6] :: [Int]
+-- [0,0,0,1,1,2,3,3,4,4,5,6,7,9,10]
+--
 hsort :: Ord a => [a] -> [a]
 hsort xs = go (foldr enqueue empty xs)
   where
