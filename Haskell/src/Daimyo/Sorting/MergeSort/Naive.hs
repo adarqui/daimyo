e10741dfb23b0ec7d0bf2af6bc123cc028c915c8
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-24 18:28:58 -0400

    correction

--- Haskell/src/Daimyo/Sorting/MergeSort/Naive.hs
+++ Haskell/src/Daimyo/Sorting/MergeSort/Naive.hs
@@ -7,6 +7,9 @@ module Daimyo.Sorting.MergeSort.Naive (
 -- >>> msort [1,7,4,3,0,9] :: [Int]
 -- [0,1,3,4,7,9]
 --
+-- >>> msort [1,3,1,9,4,2,3,0,4,5,10,0,0,7,6] :: [Int]
+-- [0,0,0,1,1,2,3,3,4,4,5,6,7,9,10]
+--
 msort :: Ord a => [a] -> [a]
 msort []  = []
 msort [x] = [x]
@@ -17,6 +20,6 @@ msort xs  = merge (msort xs1) (msort xs2)
     k   = length xs `div` 2
     merge [] zs = zs
     merge ys [] = ys
-    merge (y:ys) (z:zs)
-      | y < z     = y : z : merge ys zs
-      | otherwise = z : y : merge ys zs
+    merge a@(y:ys) b@(z:zs)
+      | y < z     = y : merge ys b
+      | otherwise = z : merge a zs
