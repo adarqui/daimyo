c7dca0c135f07cff2ce96a9916d1debc3d70b91f
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-24 18:25:52 -0400

    variations

--- Haskell/src/Daimyo/Sorting/QuickSort/Naive.hs
+++ Haskell/src/Daimyo/Sorting/QuickSort/Naive.hs
@@ -1,5 +1,7 @@
 module Daimyo.Sorting.QuickSort.Naive (
-  qsort
+  qsort,
+  qsort1,
+  qsort2
 ) where
 
 -- | qsort
@@ -8,6 +10,33 @@ module Daimyo.Sorting.QuickSort.Naive (
 -- [0,1,3,4,7,9]
 --
 qsort :: Ord a => [a] -> [a]
-qsort [] = []
-qsort (pivot:rest) =
-  qsort [ x | x <- rest, x <= pivot] ++ [pivot] ++ qsort [ x | x <- rest, x > pivot ]
+qsort = go []
+  where
+    go acc []           = acc
+    go acc (pivot:rest) = go (pivot : (go acc upper)) lower
+      where
+        lower = [ x | x <- rest, x <= pivot ]
+        upper = [ x | x <- rest, x > pivot  ]
+
+-- | qsort1
+--
+-- >>> qsort1 [1,7,4,3,0,9] :: [Int]
+-- [0,1,3,4,7,9]
+--
+qsort1 :: Ord a => [a] -> [a]
+qsort1 []           = []
+qsort1 (pivot:rest) = lh ++ middle ++ rh
+  where
+    lh = qsort1 [ x | x <- rest, x <= pivot]
+    middle = [pivot]
+    rh = qsort1 [ x | x <- rest, x > pivot ]
+
+-- | qsort2
+--
+-- >>> qsort2 [1,7,4,3,0,9] :: [Int]
+-- [0,1,3,4,7,9]
+--
+qsort2 :: Ord a => [a] -> [a]
+qsort2 []           = []
+qsort2 (pivot:rest) =
+  qsort2 [ x | x <- rest, x <= pivot] ++ [pivot] ++ qsort2 [ x | x <- rest, x > pivot ]
