be915906502a15dd9bda29cbed87ef946c283a1b
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-26 04:27:41 -0400

    some examples of course dependency graph

---
+++ Haskell/src/Daimyo/Graph/Array/Examples.hs
@@ -0,0 +1,26 @@
+module Daimyo.Graph.Array.Examples (
+  course_dependencies
+) where
+
+import           Daimyo.Graph.Array
+import           GHC.Arr
+
+data Courses
+  = Math
+  | Theory
+  | Languages
+  | Programming
+  | Concurrency
+  | Architecture
+  | Parallelism
+  deriving (Eq, Ord, Enum, Ix, Show)
+
+course_dependencies = tsort $ mkGraph (Math, Parallelism)
+  [
+    (Math,Theory,1),
+    (Languages,Theory,1),
+    (Programming,Languages,1),
+    (Programming,Concurrency,1),
+    (Concurrency,Parallelism,1),
+    (Architecture,Parallelism,1)
+  ]
