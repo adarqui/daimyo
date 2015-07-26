c85b177d5867fc6bf8d0e9491d8a601d89634ba3
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-26 05:07:48 -0400

    selection sort divide and conquer

---
+++ Haskell/src/Daimyo/Sorting/SelectionSort/DivideAndConquer.hs
@@ -0,0 +1,20 @@
+module Daimyo.Sorting.SelectionSort.DivideAndConquer (
+  ssort
+) where
+
+import           Daimyo.Algorithm.TopDown.DivideAndConquer
+import           Data.List
+
+-- | ssort
+--
+-- >>> ssort [1,3,5,2,0,9,4,7] :: [Int]
+-- [0,1,2,3,4,5,7,9]
+--
+ssort :: Ord a => [a] -> [a]
+ssort xs = divideAndConquer ind solve divide combine xs
+  where
+    ind xs    = length xs <= 1
+    solve     = id
+    divide xs = [ [m], delete m xs ]
+      where m = minimum xs
+    combine _ (l1:l2:[]) = l1 ++ l2
