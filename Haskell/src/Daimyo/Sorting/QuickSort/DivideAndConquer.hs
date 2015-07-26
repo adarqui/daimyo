20892338d5504efc90c4ee70736ad4943bdd6ea4
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-26 04:53:54 -0400

    quicksort divide and conquer

---
+++ Haskell/src/Daimyo/Sorting/QuickSort/DivideAndConquer.hs
@@ -0,0 +1,18 @@
+module Daimyo.Sorting.QuickSort.DivideAndConquer (
+  qsort
+) where
+
+import           Daimyo.Algorithm.TopDown.DivideAndConquer
+
+-- | qsort
+--
+-- >>> qsort [1,3,5,2,0,9,4,7] :: [Int]
+-- [0,1,2,3,4,5,7,9]
+--
+qsort :: Ord a => [a] -> [a]
+qsort xs = divideAndConquer ind solve divide combine xs
+  where
+    ind xs        = length xs <= 1
+    solve         = id
+    divide (x:xs) = [ [y|y<-xs, y<=x], [y|y<-xs, y>x] ]
+    combine (x:_) (l1:l2:[]) = l1 ++ [x] ++ l2
