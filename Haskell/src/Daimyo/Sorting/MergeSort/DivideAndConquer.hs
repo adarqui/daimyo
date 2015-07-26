52c7ed0040a35c414c484c78cda57a8b56766ab6
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-26 04:42:39 -0400

    msort

--- Haskell/src/Daimyo/Sorting/MergeSort/DivideAndConquer.hs
+++ Haskell/src/Daimyo/Sorting/MergeSort/DivideAndConquer.hs
@@ -1,11 +1,15 @@
 module Daimyo.Sorting.MergeSort.DivideAndConquer (
-  msort
+  msort,
+  msortReverse
 ) where
 
 import           Daimyo.Algorithm.TopDown.DivideAndConquer
 import qualified Daimyo.Sorting.MergeSort.Naive            as M
 
 -- | msort
+--
+-- >>> msort [1,3,5,2,0,9,4,7] :: [Int]
+-- [0,1,2,3,4,5,7,9]
 --
 msort :: Ord a => [a] -> [a]
 msort xs = divideAndConquer ind solve divide combine xs
@@ -15,3 +19,17 @@ msort xs = divideAndConquer ind solve divide combine xs
     divide xs = [take n xs, drop n xs]
       where n = length xs `div` 2
     combine _ (l1:l2:[]) = M.merge l1 l2
+
+-- | msortReverse
+--
+-- >>> msortReverse [1,3,5,2,0,9,4,7] :: [Int]
+-- [7,4,9,3,1,5,2,0]
+--
+msortReverse :: Ord a => [a] -> [a]
+msortReverse xs = divideAndConquer ind solve divide combine xs
+  where
+    ind xs    = length xs <= 1
+    solve     = id
+    divide xs = [take n xs, drop n xs]
+      where n = length xs `div` 2
+    combine _ (l1:l2:[]) = reverse $ M.merge l1 l2
