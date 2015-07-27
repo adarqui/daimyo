8fe0857609d4e773528fa7dc4e54de608f095b52
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-27 00:37:24 -0400

    palindrome cleanup

--- Haskell/src/Daimyo/Patterns/Palindrome.hs
+++ Haskell/src/Daimyo/Patterns/Palindrome.hs
@@ -1,37 +1,23 @@
 module Daimyo.Patterns.Palindrome (
- makePalindrome,
- makePalindromeX
+  makePalindrome
 ) where
 
-import Data.Char
-import Control.Monad
-
-main :: IO ()
-main = do
- _T <- readLn :: IO Int
- strings <- replicateM _T getLine
- mapM_ (putStrLn . show . makePalindrome) strings
-
-makePalindrome s = makePalindrome' s (reverse s)
-
-makePalindrome' _ [] = 0
-makePalindrome' s@(sx:ss) rs@(rsx:rss) = (diff sx rsx) + makePalindrome' ss rss
-
-diff sc rc
- | sc >= rc = 0
- | sc < rc = (ord rc - ord sc)
-
-makePalindromeX s = makePalindromeX' s (reverse s)
-
-makePalindromeX' _ [] = []
-makePalindromeX' s@(sx:ss) rs@(rsx:rss) = (diffX sx rsx) : makePalindromeX' ss rss
-
-diffX sc rc
- | sc >= rc = rc
- | sc < rc = sc
-
-t1 = makePalindrome "abc"
-t2 = makePalindrome "abcba"
-t3 = makePalindrome "abcd"
-t4 = makePalindrome "eeeeaaaa"
-t5 = makePalindrome "eeeeeeea"
+-- | makePalindrome
+--
+-- >>> makePalindrome "abc"
+-- "aba"
+--
+-- >>> makePalindrome "abcba"
+-- "abcba"
+--
+-- >>> makePalindrome "eeeeaaaa"
+-- "aaaaaaaa"
+--
+makePalindrome :: Ord t => [t] -> [t]
+makePalindrome s = go s (reverse s)
+  where
+    go _ [] = []
+    go (x:xs) (y:ys) = diff x y : go xs ys
+    diff x y
+      | x >= y = y
+      | x < y  = x
