1c85d22f739c5d4e3b009f4769b3b5f3096d24ce
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-26 22:41:18 -0400

    statistics semi cleanup

--- Haskell/src/Daimyo/Statistics.hs
+++ Haskell/src/Daimyo/Statistics.hs
@@ -1,119 +1,130 @@
-{-# LANGUAGE RecordWildCards, ParallelListComp #-}
+{-# LANGUAGE ParallelListComp #-}
+{-# LANGUAGE RecordWildCards  #-}
 
-{-
-    source: http://integral-table.com/downloads/stats.pdf
--}
+--
+-- source: http://integral-table.com/downloads/stats.pdf
+--
 
 module Daimyo.Statistics (
-    Population,
-    PopulationStats,
-    fromList,
-    fromListStats,
-    mean,
-    mean'sample,
-    variance,
-    variance'sample,
-    stddev,
-    stddev'sample,
-    zscore'sample,
-    correlation'sample,
-    µ,
-    σ2,
-    σ,
-    xbar,
-    s2x,
-    sx,
-    z,
-    r,
-    t_dogs
+  Population,
+  PopulationStats,
+  fromList,
+  fromListStats,
+  mean,
+  meanSample,
+  variance,
+  varianceSample,
+  stddev,
+  stddevSample,
+  zscoreSample,
+  correlationSample,
+  µ,
+  σ2,
+  σ,
+  xbar,
+  s2x,
+  sx,
+  z,
+  r
 ) where
 
-import Data.List
+import           Data.List
 
+-- | Population
+--
 data Population a = Population {
-    set :: [a],
-    n :: Double
+  popSet :: [a],
+  popN   :: Double
 } deriving (Show)
 
+-- | PopulationStats
+--
 data PopulationStats a = PopulationStats {
-    _pop :: Population a,
-    _mean :: Double,
-    _variance :: Double,
-    _stddev :: Double
+  popStatsPop      :: Population a,
+  popStatsMean     :: Double,
+  popStatsVariance :: Double,
+  popStatsStddev   :: Double
 } deriving (Show)
 
+-- | fromList
+--
+-- >>> fromList ([600, 470, 170, 430, 300] :: [Int])
+-- Population {popSet = [600,470,170,430,300], popN = 5.0}
+--
+fromList :: [a] -> Population a
 fromList l = Population {
-    set = l,
-    n = fromIntegral (length l) :: Double
+  popSet = l,
+  popN   = fromIntegral (length l) :: Double
 }
 
-fromListStats l =
-    let
-        pop = fromList l
-    in
-        PopulationStats {
-            _pop = pop,
-            _mean = mean pop,
-            _variance = variance pop,
-            _stddev = stddev pop
-        }
+-- | fromListStats
+--
+-- >>> fromListStats  ([600, 470, 170, 430, 300] :: [Double])
+-- PopulationStats {popStatsPop = Population {popSet = [600.0,470.0,170.0,430.0,300.0], popN = 5.0}, popStatsMean = 394.0, popStatsVariance = 21704.0, popStatsStddev = 147.32277488562318}
+--
+fromListStats :: [Double] -> PopulationStats Double
+fromListStats xs =
+  PopulationStats {
+    popStatsPop = pop,
+    popStatsMean = mean pop,
+    popStatsVariance = variance pop,
+    popStatsStddev = stddev pop
+  }
+  where
+    pop = fromList xs
 
-mean Population{..} = (1/n) * sum set
+-- | mean
+--
+mean :: Population Double -> Double
+mean Population{..} = (1/popN) * sum popSet
 
-mean'sample Population{..} = (1/n) * sum set
+-- | meanSample
+--
+meanSample :: Population Double -> Double
+meanSample Population{..} = (1/popN) * sum popSet
 
-variance p@Population{..} =
-    let
-        smean = mean'sample p
-        set' = map (\i -> (i - smean)**2) set
-    in
-        (1/n) * sum set'
+-- | variance
+--
+variance :: Population Double -> Double
+variance p@Population{..} = (1/popN) * sum set'
+  where
+    smean = meanSample p
+    set'  = map (\i -> (i - smean)**2) popSet
 
-variance'sample p@Population{..} =
-    let
-        smean = mean'sample p
-        set' = map (\i -> (i - smean)**2) set
-    in
-        (1/(n-1)) * sum set'
+-- | varianceSample
+--
+varianceSample :: Population Double -> Double
+varianceSample p@Population{..} = (1/(popN-1)) * sum set'
+  where
+    smean = meanSample p
+    set'  = map (\i -> (i - smean)**2) popSet
 
-stddev p@Population{..} =
-    let
-        var = variance p
-    in
-        sqrt var
+-- | stddev
+--
+stddev :: Population Double -> Double
+stddev = sqrt . variance
 
-stddev'sample p@Population{..} =
-    let
-        var = variance'sample p
-    in
-        sqrt var
+-- | stddevSample
+--
+stddevSample :: Population Double -> Double
+stddevSample = sqrt . varianceSample
 
-zscore'sample x p@Population{..} = (x - (mean p)) / (stddev p) 
+-- | zscoreSample
+--
+zscoreSample :: Double -> Population Double -> Double
+zscoreSample x p = (x - (mean p)) / (stddev p)
 
-correlation'sample p q =
-    (1/((n p)-1)) * (sum [ ((xi - (xbar p)) / (sx p)) * ((yi - (xbar p)) / (sx q)) | xi <- set p | yi <- set q])
+-- | correlationSample
+--
+correlationSample :: Population Double -> Population Double -> Double
+correlationSample p q =
+    (1/((popN p)-1)) * (sum [ ((xi - (xbar p)) / (sx p)) * ((yi - (xbar p)) / (sx q)) | xi <- popSet p | yi <- popSet q])
 
 (µ) = mean
 (σ2) = variance
 (σ) = stddev
-xbar = mean'sample
-s2x = variance'sample
-sx = stddev'sample
-z = zscore'sample
-r = correlation'sample
-
-{-
-    dogs: http://www.mathsisfun.com/data/standard-deviation.html
--}
-
-t_dogs = do
-    let p = fromList [600, 470, 170, 430, 300]
-    let q = fromList [601, 470, 160, 400, 350]
-    putStrLn $ "population mean: " ++ (show $ mean p)
-    putStrLn $ "population variance: " ++ (show $ variance p)
-    putStrLn $ "population standard deviation: " ++ (show $ stddev p)
-    putStrLn $ "sample mean: " ++ (show $ mean'sample p)
-    putStrLn $ "sample variance: " ++ (show $ variance'sample p)
-    putStrLn $ "sample standard deviation: " ++ (show $ stddev'sample p)
-    putStrLn $ "sample z-score for 150: " ++ (show $ zscore'sample 150 p)
-    putStrLn $ "sample correlation between p & q: " ++ (show $ correlation'sample p q)
+xbar = meanSample
+s2x = varianceSample
+sx = stddevSample
+z = zscoreSample
+r = correlationSample
