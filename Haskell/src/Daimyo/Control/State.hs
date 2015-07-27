2a89364d53fd699b62ff24cca98dde13ea03bad7
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-27 00:05:01 -0400

    state cleanup

--- Haskell/src/Daimyo/Control/State.hs
+++ Haskell/src/Daimyo/Control/State.hs
@@ -1,44 +1,53 @@
-module Daimyo.State (
-    State (..),
-    evalState,
-    execState,
-    get,
-    put,
-    modify
+module Daimyo.Control.State (
+  State (..),
+  evalState,
+  execState,
+  get,
+  put,
+  modify
 ) where
 
+-- | State
+--
 newtype State s a = State { runState :: s -> (a, s) }
 
+-- | evalState
+--
 evalState :: State s a -> s -> a
 evalState m s = fst $ runState m s
 
+-- | execState
+--
 execState :: State s a -> s -> s
 execState m s = snd $ runState m s
 
+-- | get
+--
+get :: State a a
 get = State $ \s -> (s, s)
+
+-- | put
+--
+put :: s -> State s ()
 put s = State $ \_ -> ((), s)
-modify f s = State $ \s' -> ((), f s')
+
+-- | modify
+--
+modify :: (s -> s) ->  State s ()
+modify f = State $ \s' -> ((), f s')
 
 instance Functor (State s) where
-    fmap f m = State $ \s ->
-        let
-            (a, s') = runState m s
-        in
-            (f a, s')
+  fmap f m = State $ \s -> let (a, s') = runState m s in (f a, s')
 
 instance Monad (State s) where
-    return a = State $ \s -> (a, s)
-    m >>= k = State $ \s ->
-        let
-            (a, s') = runState m s
-        in
-            runState (k a) s'
+  return a = State $ \s -> (a, s)
+  m >>= k  = State $ \s -> let (a, s') = runState m s in runState (k a) s'
 
-t = (put 6 >> modify (+1) get >> get)
-t1 = evalState t (5 :: Int)
-t2 = execState t (5 :: Int)
-t3 = runState t (5:: Int)
-t4 = runState (put 6 >> get) (5 :: Int)
-t5 = runState (State $ \s -> (1,s)) 2
-t6 = runState (let st = (State $ \s -> (1,s)) in st) 2
-t7 = runState (let st = (State $ \s -> (1,s)) in st) 2
+-- | Examples
+--
+-- >>> runState (modify (+1) >> modify (+1)) (5 :: Int)
+-- ((),7)
+--
+-- >>> runState (modify (+1) >> modify (+1) >> get) (5 :: Int)
+-- (7,7)
+--
