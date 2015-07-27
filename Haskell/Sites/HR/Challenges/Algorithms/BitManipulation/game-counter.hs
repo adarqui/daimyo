4cb947a9b5c82a58c84b5fa058937ffad3cd4f83
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-27 14:55:07 -0400

    game-counter cleanup

--- Haskell/Sites/HR/Challenges/Algorithms/BitManipulation/game-counter.hs
+++ Haskell/Sites/HR/Challenges/Algorithms/BitManipulation/game-counter.hs
@@ -1,38 +1,67 @@
-{-
- - source: https://www.hackerrank.com/challenges/counter-game
- -}
-
 import           Control.Monad
 import           Data.Bits     hiding (rotate)
 
-data Player = Louise | Richard deriving (Show, Read, Eq, Enum)
+data Player
+  = Louise
+  | Richard
+  deriving (Show, Read, Eq, Enum)
 
-main :: IO ()
-main = do
- _T <- readLn :: IO Int
- nums <- mapM (\_ -> readLn :: IO Integer) [1.._T]
- mapM_ (\n -> putStrLn $ show $ game n) nums
-
+-- | game
+--
 game :: Integer -> Player
 game n
-  | n == 1 = Richard
+  | n == 1    = Richard
   | otherwise = game' Louise n
 
+-- | game'
+--
 game' :: Player -> Integer -> Player
 game' player 1 = rotate player
 game' player n =
- case (isPowerOfTwo'ComplementAndCompare n) of
-  False -> game' rot (n - floorPowerOfTwo n)
-  True -> game' rot (n `div` 2)
- where
-  rot = rotate player
+ if (isPowerOfTwo_ComplementAndCompare n)
+    then game' (rotate player) (n - floorPowerOfTwo n)
+    else game' (rotate player) (n `div` 2)
 
+-- | rotate
+--
 rotate :: Player -> Player
-rotate Louise = Richard
+rotate Louise  = Richard
 rotate Richard = Louise
 
-ceilPowerOfTwo n = 1 + foldl (\acc i -> acc .|. (acc `shiftR` i)) (n - 1) [1,2,4,8,16,32,64]
+-- | ceilPowerOfTwo
+--
+-- >>> ceilPowerOfTwo 5
+-- 8
+ceilPowerOfTwo :: (Bits a, Num a) => a -> a
+ceilPowerOfTwo n = 1 + foldl f (n - 1) [1,2,4,8,16,32,64]
+  where f acc i = acc .|. (acc `shiftR` i)
 
+-- | floorPowerOfTwo
+--
+-- >>> floorPowerOfTwo 5
+-- 4
+--
+--  >>> floorPowerOfTwo 9
+-- 8
+--
+floorPowerOfTwo :: (Bits a, Num a) => a -> a
 floorPowerOfTwo n = (ceilPowerOfTwo n) `shiftR` 1
 
-isPowerOfTwo'ComplementAndCompare x = (x /= 0) && ((x .&. (complement x + 1)) == x)
+-- | isPowerOfTwo_ComplementAndCompare
+--
+-- >>> isPowerOfTwo_ComplementAndCompare 5
+-- False
+--
+-- >>> isPowerOfTwo_ComplementAndCompare 8
+-- True
+--
+isPowerOfTwo_ComplementAndCompare :: (Bits a, Num a) => a -> Bool
+isPowerOfTwo_ComplementAndCompare x = (x /= 0) && ((x .&. (complement x + 1)) == x)
+
+-- | main
+--
+main :: IO ()
+main = do
+  test_cases <- readLn :: IO Int
+  nums <- fmap (map read . lines) getContents
+  mapM_ (putStrLn . show . game) nums
