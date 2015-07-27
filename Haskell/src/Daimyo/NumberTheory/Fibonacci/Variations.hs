0f03d77b87457df9c4b14ee888f8ee28501daad1
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-27 19:13:40 -0400

    fixes

--- Haskell/src/Daimyo/NumberTheory/Fibonacci/Variations.hs
+++ Haskell/src/Daimyo/NumberTheory/Fibonacci/Variations.hs
@@ -13,53 +13,55 @@ import           GHC.Arr
 -- | fib3'
 --
 -- >>> fib3' 5 :: Int
--- 7
+-- 11
 --
 -- >>> fib3' 10 :: Int
--- 149
+-- 230
 --
 fib3' :: Integral a => a -> a
 fib3' n
-  | n <= 0    = 0
-  | n == 1    = 1
+  | n <= 3    = n
   | otherwise = fib3' (n-1) + fib3' (n-2) + fib3' (n-3)
 
 -- | fib3'
 --
 -- >>> fib4' 5 :: Int
--- 8
+-- 10
 --
 -- >>> fib4' 10 :: Int
--- 208
+-- 258
 --
 fib4' :: Integral a => a -> a
 fib4' n
-  | n <= 0    = 0
-  | n == 1    = 1
+  | n <= 4    = n
   | otherwise = fib4' (n-1) + fib4' (n-2) + fib4' (n-3) + fib4' (n-4)
 
 -- | compFib3
 --
 compFib3 :: (Ix a, Integral a) => Table a a -> a -> a
 compFib3 t i
-  | i < 0     = 0
-  | i <= 1    = i
+  | i <= 2    = i
   | otherwise = findTable (i-1) t + findTable (i-2) t + findTable (i-3) t
 
 -- | compFib4
 --
 compFib4 :: (Ix a, Integral a) => Table a a -> a -> a
 compFib4 t i
-  | i < 0     = 0
-  | i <= 1    = i
+  | i <= 4    = i
   | otherwise = findTable (i-1) t + findTable (i-2) t + findTable (i-3) t + findTable (i-4) t
 
 -- | fib3
+--
+-- >>> fib3 10 :: Int
+-- 230
 --
 fib3 :: (Ix a, Integral a) => a -> a
 fib3 = fibDp compFib3
 
 -- | fib4
+--
+-- >>> fib4 10 :: Int
+-- 258
 --
 fib4 :: (Ix a, Integral a) => a -> a
 fib4 = fibDp compFib4
