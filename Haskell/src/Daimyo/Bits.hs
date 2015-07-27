f69272388b0626d494a8b1f8b33ffb381cf99dec
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-27 14:57:40 -0400

    some bit functions from an h-rank challenge

--- Haskell/src/Daimyo/Bits.hs
+++ Haskell/src/Daimyo/Bits.hs
@@ -5,10 +5,14 @@ module Daimyo.Bits (
   intersperseL,
   splits,
   balance,
-  fromList
+  fromList,
+  ceilPowerOfTwo,
+  floorPowerOfTwo,
+  isPowerOfTwo_ComplementAndCompare
 ) where
 
 import qualified Data.Bits as Bits
+import           Data.Bits
 import           Data.List
 
 -- | Tree
@@ -91,3 +95,34 @@ charToBits char = map f [0..7]
     f bit
       | Bits.testBit char bit = 1
       | otherwise             = 0
+
+-- | ceilPowerOfTwo
+--
+-- >>> ceilPowerOfTwo 5
+-- 8
+--
+ceilPowerOfTwo :: (Bits a, Num a) => a -> a
+ceilPowerOfTwo n = 1 + foldl f (n - 1) [1,2,4,8,16,32,64]
+  where f acc i = acc .|. (acc `shiftR` i)
+
+-- | floorPowerOfTwo
+--
+-- >>> floorPowerOfTwo 5
+-- 4
+--
+--  >>> floorPowerOfTwo 9
+-- 8
+--
+floorPowerOfTwo :: (Bits a, Num a) => a -> a
+floorPowerOfTwo n = (ceilPowerOfTwo n) `shiftR` 1
+
+-- | isPowerOfTwo_ComplementAndCompare
+--
+-- >>> isPowerOfTwo_ComplementAndCompare 5
+-- False
+--
+-- >>> isPowerOfTwo_ComplementAndCompare 8
+-- True
+--
+isPowerOfTwo_ComplementAndCompare :: (Bits a, Num a) => a -> Bool
+isPowerOfTwo_ComplementAndCompare x = (x /= 0) && ((x .&. (complement x + 1)) == x)
