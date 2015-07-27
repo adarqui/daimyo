cd55cbcb0d4350ea6c95bd4b03c9955f745433db
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-27 00:52:26 -0400

    number cleanup.. digits function is disgusting

--- Haskell/src/Daimyo/Number.hs
+++ Haskell/src/Daimyo/Number.hs
@@ -1,78 +1,140 @@
 module Daimyo.Number (
-    Number (..),
-    isInteger,
-    isWhole,
-    isCounting,
-    number,
-    decimalPart,
-    decimalPart'to'Integer,
-    round',
-    by10,
-    one'by10,
-    digits,
-    digits'to'num
+  Number (..),
+  isInteger,
+  isWhole,
+  isCounting,
+  number,
+  decimalPart,
+  decimalPartToInteger,
+  round',
+  by10,
+  oneBy10,
+  digits,
+  digitsToNumber
 ) where
 
-import Data.List
-import Data.Maybe
+import           Data.List
+import           Data.Maybe
 
+-- | Number
+--
 data Number = Number {
-    n :: Double,
-    integer :: Integer,
+    n          :: Double,
+    integer    :: Integer,
     fractional :: Integer,
-    decimal :: Double
+    decimal    :: Double
 } deriving (Show, Read, Eq)
 
+-- | isInteger
+--
+-- >>> isInteger (5 :: Double)
+-- True
+--
+-- >>> isInteger (-5 :: Double)
+-- True
+--
+-- >>> isInteger (5.5 :: Double)
+-- False
+--
+isInteger :: RealFrac a => a -> Bool
 isInteger x = floor x == ceiling x
 
+-- | isWhole
+--
+-- >>> isWhole (-5 :: Double)
+-- False
+--
+-- >>> isWhole (5 :: Double)
+-- True
+--
+isWhole :: RealFrac a => a -> Bool
 isWhole x = x >= 0 && isInteger x
 
+-- | isCounter
+--
+isCounting :: RealFrac a => a -> Bool
 isCounting x = x > 0 && isInteger x
 
-{- precision life.. -}
-
+-- | number
+--
+-- >>> number 5.5
+-- Number {n = 5.5, integer = 5, fractional = 5, decimal = 0.5}
+--
 number :: Double -> Number
 number n' = Number {
-    n = n',
-    integer = truncate n',
-    fractional = decimalPart'to'Integer n',
-    decimal = decimalPart n'
+  n          = n',
+  integer    = truncate n',
+  fractional = decimalPartToInteger n',
+  decimal    = decimalPart n'
 }
 
+-- | decimalPart
+--
+-- >>> decimalPart 5.5
+-- 0.5
+--
+decimalPart :: Double -> Double
 decimalPart n = n - (fromIntegral (floor n) :: Double)
 
-decimalPart'to'Integer n = decimalPart'to'Integer' (decimalPart n)
-decimalPart'to'Integer' n
-    | isInteger n = truncate n
-    | otherwise = decimalPart'to'Integer' (n * 10)
+-- | decimalPartToInteger
+--
+-- >>> decimalPartToInteger 5.5 :: Int
+-- 5
+--
+decimalPartToInteger :: Integral b => Double -> b
+decimalPartToInteger n = decimalPartToInteger' (decimalPart n)
+decimalPartToInteger' n
+  | isInteger n = truncate n
+  | otherwise   = decimalPartToInteger' (n * 10)
 
 round' f n =  (fromInteger $ round $ f * (10^n)) / (10.0^^n)
 round'' f n =  (round $ f * (10^n)) / (10.0^^n)
 
+-- | by10
+--
+-- >>> ake 5 by10
+-- [10,100,1000,10000,100000]
+--
+by10 :: [Integer]
 by10 = scanl (\acc y -> acc * 10) 10 [1..]
-one'by10 = 1 : by10
 
-{-
-    extracts the digits from an integer
--}
+-- | oneBy10
+--
+-- >>> take 5 oneBy10
+-- [1,10,100,1000,10000]
+--
+oneBy10 :: [Integer]
+oneBy10 = 1 : by10
 
+
+-- | digits
+--
+-- whoa this is disgusting.. atrocious
+--
+-- >>> digits (12345 :: Int) :: [Int]
+-- [1,2,3,4,5]
+--
+digits :: (Integral a, Integral b) => a -> [b]
 digits n =
-        map truncate $
-        reverse $
-        catMaybes $
-        takeWhile (/= Nothing) $
-        fst $
-        foldl (\(list, acc) _ ->
-            let
-                acc' = acc / 10.0
-                digit = round' (decimalPart acc') 1 * 10
-            in
-                if (acc == 0)
-                    then (list++[Nothing], 0)
-                    else (list++[Just digit], fromIntegral (truncate acc') :: Double)
-        ) ([],fromIntegral n :: Double) $ take 100 $ repeat 10.0
+  map truncate $
+  reverse $
+  catMaybes $
+  takeWhile (/= Nothing) $
+  fst $
+  foldl (\(list, acc) _ ->
+      let
+          acc' = acc / 10.0
+          digit = round' (decimalPart acc') 1 * 10
+      in
+          if (acc == 0)
+              then (list++[Nothing], 0)
+              else (list++[Just digit], fromIntegral (truncate acc') :: Double)
+  ) ([],fromIntegral n :: Double) $ take 100 $ repeat 10.0
 
-
-digits'to'num ds = sum $ map (\(d,t) -> d * t) $ zip (reverse ds) one'by10
-
-t_digits = digits 1992132213246009
+-- | digitsToNumber
+--
+-- >>> digitsToNumber [1,2,3,4,5]
+-- 12345
+--
+digitsToNumber :: [Integer] -> Integer
+digitsToNumber ds = sum $ map (\(d,t) -> d * t) $ zip (reverse ds) oneBy10
