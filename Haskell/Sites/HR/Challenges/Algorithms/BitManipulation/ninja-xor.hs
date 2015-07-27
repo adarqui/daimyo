0f90c6f0d0cea6569d59a6ea2303a71acc37d2c8
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-27 16:42:31 -0400

    ninja-xor cleanup

--- Haskell/Sites/HR/Challenges/Algorithms/BitManipulation/ninja-xor.hs
+++ Haskell/Sites/HR/Challenges/Algorithms/BitManipulation/ninja-xor.hs
@@ -1,25 +1,49 @@
 {-# OPTIONS -O2 #-}
+
 import           Control.Monad
 import           Data.Bits
 import           Data.List
 
-main :: IO ()
-main = do
- _T <- readLn :: IO Int
- pairs <- mapM (\_ -> getPair) [1.._T]
- mapM_ (\(n, nums) -> putStrLn $ show $ solve nums) pairs
+-- | solve
+--
+-- >>> solve [1,2,3,4]
+-- 56
+--
+solve :: [Int] -> Int
+solve nums = (foldl (\acc l -> acc + xorSum l) 0 $ subsequences nums) `mod` modp
 
-solve nums = (foldl (\acc l -> acc + xorsum l) 0 $ subsequences nums) `mod` modp
-
+-- | modp
+--
+modp :: Int
 modp = ((10^9) + 7)
 
-xorsum [] = 0
-xorsum (x:xs) = x `xor` xorsum xs
+-- | xorSum
+--
+-- >>> xorSum [1,2,3,4]
+-- 4
+--
+xorSum :: (Bits a, Num a) => [a] -> a
+xorSum [] = 0
+xorSum (x:xs) = x `xor` xorSum xs
 
+-- | getPair
+--
 getPair :: IO (Int,[Int])
 getPair = do
  n <- readLn :: IO Int
  nums <- getLine >>= \line -> return $ map (\n -> read n :: Int) $ words line
  return (n, nums)
 
+-- | subseqs
+--
+-- >>> subseqs [1,2,3]
+-- [[1],[1,2],[2],[1,2,3],[2,3],[3]]
+--
+subseqs :: [a] -> [[a]]
 subseqs l = [x | i <- inits l, x <- tails i, not $ null x ]
+
+main :: IO ()
+main = do
+  test_cases <- readLn :: IO Int
+  pairs <- mapM (const getPair) [1..test_cases]
+  mapM_ (putStrLn . show . solve . snd) pairs
