7bec29b35b668ce234f30f6e3d95d02851fbbc1b
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-28 01:40:34 -0400

    super digit

---
+++ Haskell/Sites/HR/Challenges/FunctionalProgramming/Recursion/super-digit.hs
@@ -0,0 +1,33 @@
+import Data.List
+
+-- | digits
+--
+-- >>> digits 1234 :: [Int]
+-- [1,2,3,4]
+--
+digits :: Integral a => a -> [a]
+digits = go []
+  where
+    go acc 0 = acc
+    go acc n = go (n `mod` 10 : acc) (n `div` 10)
+
+-- | solution
+--
+-- >>> solution 148 3
+-- 3
+--
+-- >>> solution 9875 1
+-- 2
+--
+solution :: Int -> Int -> Int
+solution number replication = go initial
+  where
+    initial = concat $ replicate replication (digits number)
+    go list
+      | length list == 1 = head list
+      | otherwise        = go (digits (sum list))
+
+main :: IO ()
+main = do
+  [number, replication] <- fmap (map read . words) getLine
+  putStrLn $ show $ solution number replication
