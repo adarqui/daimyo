8faa640ee3994a4c7e0f1fec2a44e8aca5accd0c
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-29 00:39:41 -0400

    additive primes cleanup

--- Haskell/src/Daimyo/NumberTheory/Prime/Additive.hs
+++ Haskell/src/Daimyo/NumberTheory/Prime/Additive.hs
@@ -1,11 +1,26 @@
 module Daimyo.NumberTheory.Prime.Additive (
-    isPrimeDigits,
-    additive
+  isPrimeDigits,
+  additivePrimes
 ) where
 
-import Daimyo.Number
-import Daimyo.NumberTheory.Prime
+import           Daimyo.Number
+import           Daimyo.NumberTheory.Prime
 
-additive = filter isPrimeDigits primes
+-- | additivePrimes
+--
+-- >>> take 20 additivePrimes
+-- [2,3,5,7,11,23,29,41,43,47,61,67,83,89,101,113,131,137,139,151]
+--
+additivePrimes :: [Integer]
+additivePrimes = filter isPrimeDigits primes
 
-isPrimeDigits n = isPrime $ sum $ digits n
+-- | isPrimeDigits
+--
+-- >>> isPrimeDigits 131
+-- True
+--
+-- >>> isPrimeDigits 132
+-- False
+--
+isPrimeDigits :: Integer -> Bool
+isPrimeDigits = isPrime . sum . digits
--- Haskell/src/Daimyo/NumberTheory/Prime/Circular.hs
+++ Haskell/src/Daimyo/NumberTheory/Prime/Circular.hs
@@ -1,17 +1,17 @@
 module Daimyo.NumberTheory.Prime.Circular (
-  circular
+  circularPrimes
 ) where
 
 import           Daimyo.List.Pattern
 import           Daimyo.Number
 import           Daimyo.NumberTheory.Prime
 
--- | circular
+-- | circularPrimes
 --
 -- https://en.wikipedia.org/wiki/Circular_prime
 --
--- >>> take 20 circular
+-- >>> take 20 circularPrimes
 -- [2,3,5,7,11,13,17,31,37,71,73,79,97,113,131,197,199,311,337,373]
 --
-circular :: [Integer]
-circular = filter (all (isPrime . digitsToNumber) . (rotations . digits)) primes
+circularPrimes :: [Integer]
+circularPrimes = filter (all (isPrime . digitsToNumber) . (rotations . digits)) primes
