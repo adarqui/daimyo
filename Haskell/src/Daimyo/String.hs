9227224452715d4fa1aef077b6c5a63a4377211e
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-29 00:25:05 -0400

    string cleanup

--- Haskell/src/Daimyo/String.hs
+++ Haskell/src/Daimyo/String.hs
@@ -8,9 +8,9 @@ module Daimyo.String (
 -- ("hey","sup")
 --
 split :: Eq t => t -> [t] -> ([t], [t])
-split c s = go [] c s
+split c s = go [] s
   where
-    go _ _ []     = ([], [])
-    go acc c (x:xs)
+    go _ []       = ([], [])
+    go acc (x:xs)
       | c == x    = (reverse acc, xs)
-      | otherwise = go (x:acc) c xs
+      | otherwise = go (x:acc) xs
