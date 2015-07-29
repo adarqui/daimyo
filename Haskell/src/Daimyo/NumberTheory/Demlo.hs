eb874c21b84eb1d3c5cdc96a43b15020e8a7c966
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-29 00:24:57 -0400

    demlo numbers are cool, cleanup

--- Haskell/src/Daimyo/NumberTheory/Demlo.hs
+++ Haskell/src/Daimyo/NumberTheory/Demlo.hs
@@ -1,37 +1,46 @@
 {-# LANGUAGE ParallelListComp #-}
 
 module Daimyo.NumberTheory.Demlo (
-    demlo,
-    demlos,
-    demlo'ones,
-    demlo'one
+  demlo,
+  demlos,
+  demloOnes,
+  demloOne
 ) where
 
 import Daimyo.Number
 import Data.List
 
-{-
-    really cool.
+-- | demlos
+--
+-- Demlo numbers: really cool.
+--
+-- >>> take 15 demlos
+-- [121,12321,1234321,123454321,12345654321,1234567654321,123456787654321,12345678987654321,1234567900987654321,123456790120987654321,12345679012320987654321,1234567901234320987654321,123456790123454320987654321,12345679012345654320987654321,1234567901234567654320987654321]
+--
+demlos :: [Integer]
+demlos = tail [ x*x | x <- demloOnes ]
 
-    take 15 demlos 
+-- | demloOnes
+--
+-- >>> take 10 demloOnes
+-- [1,11,111,1111,11111,111111,1111111,11111111,111111111,1111111111]
+--
+demloOnes :: [Integer]
+demloOnes = scanl (+) 1 by10
 
-[121,12321,1234321,123454321,12345654321,1234567654321,123456787654321,12345678987654321,1234567900987654321,123456790120987654321,12345679012320987654321,1234567901234320987654321,123456790123454320987654321,12345679012345654320987654321,1234567901234567654320987654321]
--}
+-- | demlo
+--
+-- >>> demlo 9
+-- 12345678987654321
+--
+demlo :: Int -> Integer
+demlo n = d*d
+  where d = demloOne n
 
-demlos = tail [ x*x | x <- demlo'ones ]
-
-
-demlo'ones = scanl (\acc x -> acc + x) 1 by10
-
-
-demlo n =
-    let
-        d = demlo'one n
-    in
-        d*d
-
-
-demlo'one n = foldl' (\acc x -> acc + x) 1 $ take (n-1) by10
-
-
-t_demlo = demlo 9 -- 12345678987654321
+-- | demloOne
+--
+-- >>> demloOne 9
+-- 111111111
+--
+demloOne :: Int -> Integer
+demloOne n = foldl' (+) 1 $ take (n-1) by10
