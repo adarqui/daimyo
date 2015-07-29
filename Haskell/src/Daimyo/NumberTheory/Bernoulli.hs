022ff2addd575db48f265c07707fb7e18cf52dd2
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-29 00:08:05 -0400

    bernoulli semi cleanup.. cant really clean this stuff up, eek

--- Haskell/src/Daimyo/NumberTheory/Bernoulli.hs
+++ Haskell/src/Daimyo/NumberTheory/Bernoulli.hs
@@ -1,82 +1,90 @@
 module Daimyo.NumberTheory.Bernoulli (
-    bernoullis,
-    bernoulli,
-    k,
-    d,
-    n,
-    z,
-    a,
-    bm,
-    bm'
+  bernoullis,
+  bernoulli,
+  k,
+  d,
+  n,
+  z,
+  a,
+  bm,
+  bm'
 ) where
 
-import Daimyo.Algebra.Divisibility
-import Daimyo.NumberTheory.Prime
-import Daimyo.NumberTheory.Factorial
-import Daimyo.NumberTheory.Constants.E
+import           Daimyo.Algebra.Divisibility
+import           Daimyo.NumberTheory.Factorial
+import           Daimyo.NumberTheory.Prime
 
+-- | bernoullis
+--
+-- >>> take 5 bernoullis
+-- [-0.5,0.16666666666666666,0.0,-4.5454545454545456e-2,0.0]
+--
+bernoullis :: [Double]
 bernoullis = [ bernoulli x | x <- [1..] ]
 
+-- | bernoulli
+--
+-- >>> bernoulli 10
+-- 7.575757575757576e-2
+--
+bernoulli :: Double -> Double
 bernoulli x = bm x
 
 
-{-
-    implement this: http://wstein.org/projects/168/kevin_mcgown/bernproj.pdf
--}
+--
+-- below implements this: http://wstein.org/projects/168/kevin_mcgown/bernproj.pdf
+--
 
+-- | k
+--
 k :: Double -> Double
 k m = (2*fac m)/((2*pi)**m)
 
-
+-- | d
+--
 d :: Double -> Double
-d m =
-    let
-        divs = divisors 50
-        divsp1 = map succ divs
-        p = filter isPrime divsp1
-    in
-        fromIntegral (product p) :: Double
+d _ = fromIntegral $ product p
+  where
+    divs   = divisors 50
+    divsp1 = map succ divs
+    p      = filter isPrime divsp1
 
-
+-- | n
+--
 n :: Double -> Double -> Double -> Double
-n m' k' d' =
-    let
-        v = ceiling $ (k'*d')**(1/(m'-1))
-    in
-        fromIntegral v :: Double
+n m' k' d' = fromIntegral v
+  where
+    v = ceiling ((k'*d')**(1/(m'-1))) :: Integer
 
-
+-- | z
+--
 z :: Double -> Double -> Double
-z m n' =
-    let
-        p = takeWhile (<=n') $ map (\p' -> fromIntegral p' :: Double) primes
-        v = map (\p' -> (1 - p'**(-m))**(-1)) p
-    in
-        product v
+z m n' = product v
+  where
+    p = takeWhile (<=n') $ map fromIntegral primes
+    v = map (\p' -> (1 - p'**(-m))**(-1)) p
 
-
+-- | a
+--
 a :: Double -> Double -> Double -> Double -> Double
-a m d' k' z' = ((-1)**(m/2+1))*(fromIntegral (ceiling(d'*k'*z')) :: Double)
+a m d' k' z' = ((-1)**(m/2+1))*(fromIntegral (ceiling (d'*k'*z') :: Integer))
 
-      
+-- | bm
+--
+bm :: Double -> Double
 bm 0 = 1
 bm 1 = -(1/2)
-bm m =
-    if (m >= 3 && odd (floor m))
-        then 0
-        else fst $ bm' m
-        
+bm m
+  | m >= 3 && odd (floor m :: Integer) = 0
+  | otherwise               = fst $ bm' m
 
-bm' m =
-    let
-        k' = k m
-        d' = d m
-        n' = n m k' d'
-        z' = z m n'
-        a' = a m k' d' z'
-    in
-        (a' / d',(k',d',n',z',a'))
-
-
-t_b50'a = 495057205241079648212477525 / 66
-t_b50'b = bm 50
+-- | bm'
+--
+bm' :: Double -> (Double, (Double, Double, Double, Double, Double))
+bm' m = (a' / d',(k',d',n',z',a'))
+  where
+  k' = k m
+  d' = d m
+  n' = n m k' d'
+  z' = z m n'
+  a' = a m k' d' z'
