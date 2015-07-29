f2161159cb81bb45ec2afc0a99257247985bf815
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-29 00:33:17 -0400

    nines

--- Haskell/src/Daimyo/NumberTheory/Nines.hs
+++ Haskell/src/Daimyo/NumberTheory/Nines.hs
@@ -1,35 +1,16 @@
 module Daimyo.NumberTheory.Nines (
-    nines
+  nines
 ) where
 
-import Daimyo.Number
-import Data.List
+import           Daimyo.Number
+import           Data.List
 
+-- | nines
+--
+-- >>> nines 999999999999999 :: Int
+-- 0
+--
+nines :: Integral a => a -> a
 nines n
-    | n < 10 = if (n == 9) then 0 else n
-    | otherwise =
-        let
-            d = digits n
-        in
-            nines (sum $ map (\x -> if (x == 9) then 0 else x) d)
-
-t_nines'1 =
-    let
-        l = [3264, 8415, 2946, 3206]
-        v = map nines l
-    in
-        (nines $ sum l, nines $ sum v, v)
-
-t_nines'2 =
-    let
-        l = [5643, 2891]
-        v = map nines l
-    in
-        (nines $ abs $ foldl' (-) 0 l, nines $ abs $ foldl' (-) 0 v, v)
-
-t_nines'3 =
-    let
-        l = [548, 629]
-        v = map nines l
-    in
-        (nines $ product l, nines $ product v, v)
+  | n < 10 = if (n == 9) then 0 else n
+  | otherwise = nines $ sum $ map (\x -> if (x == 9) then 0 else x) (digits n)
