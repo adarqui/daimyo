6099a3fb0e956029df9a18537b912ba1b3041fa9
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-31 18:18:09 -0400

    werror cleanup

--- Haskell/src/Daimyo/Graph/Array/Examples.hs
+++ Haskell/src/Daimyo/Graph/Array/Examples.hs
@@ -15,9 +15,10 @@ data Courses
   | Parallelism
   deriving (Eq, Ord, Enum, Ix, Show)
 
+course_dependencies :: [Courses]
 course_dependencies = tsort $ mkGraph (Math, Parallelism)
   [
-    (Math,Theory,1),
+    (Math,Theory,1 :: Int),
     (Languages,Theory,1),
     (Programming,Languages,1),
     (Programming,Concurrency,1),
