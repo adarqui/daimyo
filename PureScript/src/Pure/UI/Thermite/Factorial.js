2fefe2a64acbe71b448fa0fa8dc319b3c732e8d4
Andrew Darqui <andrew.darqui@gmail.com>
2015-08-02 20:21:15 -0400

    asfoksafoksa

--- PureScript/src/Pure/UI/Thermite/Factorial.js
+++ PureScript/src/Pure/UI/Thermite/Factorial.js
@@ -3,16 +3,13 @@
 // module Pure.UI.Thermite.Factorial
 
 exports.getInt = function(e) {
-  console.log(e.target.value);
   return parseInt(e.target.value);
 };
 
 exports.getValue = function(e) {
-  console.log(e.target.value);
   return e.target.value;
 };
 
 exports.getKeyCode = function(e) {
-  console.log('wtf', e.keyCode);
   return e.keyCode;
 };
