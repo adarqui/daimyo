202ebf13e3ecf953f7f8b36a75b89e9684ca2f33
Andrew Darqui <andrew.darqui@gmail.com>
2017-07-13 04:28:12 -0400

    laws!

--- rust/lib/math/modulo_arith/laws.rs
+++ rust/lib/math/modulo_arith/laws.rs
@@ -2,7 +2,7 @@
 use math::modulo;
 
 #[test]
-fn test_mod_arith_closed_addition() {
+fn test_p1_mod_arith_closed_addition() {
   /*
    * addition is closed, i,e., for any a, b in Zm, a + b in Zm
    */
@@ -10,7 +10,7 @@ fn test_mod_arith_closed_addition() {
 
 
 #[test]
-fn test_mod_arith_commutative_addition() {
+fn test_p2_mod_arith_commutative_addition() {
   /*
    * addition is commutative, i.e., for any a, b in Zm, a + b = b + a
    */
@@ -22,7 +22,7 @@ fn test_mod_arith_commutative_addition() {
 
 
 #[test]
-fn test_mod_arith_associative_addition() {
+fn test_p3_mod_arith_associative_addition() {
   /*
    * addition is associative, i.e., for any a, b, c in Zm, (a + b) + c = a + (b + c)
    */
@@ -34,7 +34,7 @@ fn test_mod_arith_associative_addition() {
 }
 
 #[test]
-fn test_mod_arith_0_additive_identity() {
+fn test_p4_mod_arith_0_additive_identity() {
   /*
    * 0 is an additive identity, i.e., for any a in Zm, a + 0 = 0 + a = a
    */
@@ -44,4 +44,88 @@ fn test_mod_arith_0_additive_identity() {
    assert_eq!(a+zero, zero+a);
    assert_eq!(a+zero, a);
    assert_eq!(a, a+zero);
+}
+
+#[test]
+fn test_p5_mod_arith_additive_inverse() {
+  /*
+   * the additive inverse of any a in Zm is m-a, i.e., a+(m-a) = (m-a)+a = 0, for any a in Zm
+   */
+   let a = 3;
+   let m = 11;
+   assert_eq!(a+(m-a), (m-a)+a);
+   assert_eq!((a+(m-a)) - ((m-a)+a), 0);
+}
+
+#[test]
+fn test_p6_mod_arith_closed_multiplication() {
+  /*
+   * multiplication is closed, i.e., for any a, b in Zm, ab in Zm
+   */
+}
+
+#[test]
+fn test_p7_mod_arith_commutative_multiplication() {
+  /*
+   * multiplication is commutative, i.e., for any a, b in Zm, ab = ba
+   */
+   let a = modulo::ModArith::new(5, 10);
+   let b = modulo::ModArith::new(77, 10);
+
+   assert_eq!(a*b, b*a);
+}
+
+#[test]
+fn test_p8_mod_arith_associative_multiplication() {
+  /*
+   * multiplication is associative, i.e., for any a, b, c in Zm, (ab)c = a(bc)
+   */
+   let a = modulo::ModArith::new(5, 10);
+   let b = modulo::ModArith::new(77, 10);
+   let c = modulo::ModArith::new(132, 10);
+
+   assert_eq!((a*b)*c, a*(b*c));
+}
+
+#[test]
+fn test_p9_mod_arith_1_multiplicative_identity() {
+  /*
+   * 1 is a multiplicative identity, i.e., for any a in Zm, a x 1 = 1 x a = a
+   */
+   let one = modulo::ModArith::new(1, 10);
+   let a = modulo::ModArith::new(5, 10);
+
+   assert_eq!(a*one, one*a);
+   assert_eq!(a*one, a);
+   assert_eq!(a, a*one);
+}
+
+#[test]
+fn test_p10_mod_arith_distributive_multiplication() {
+  /*
+   * the distributative property is satisfied, i.e., for any a, b, c in Zm, (a + b)c = (ac) + (bc) and a(b + c) = (ab) + (ac)
+   */
+   let a = modulo::ModArith::new(5, 10);
+   let b = modulo::ModArith::new(77, 10);
+   let c = modulo::ModArith::new(132, 10);
+
+   assert_eq!((a+b)*c, (a*c)+(b*c));
+   assert_eq!(a*(b+c), (a*b)+(a*c));
+}
+
+#[test]
+fn test_mod_arith_group() {
+  /*
+   * properties p1, p3, p4, p5 say that Zm forms an algebraic structure called a:
+   * group with respect to the addition property
+   *
+   * since p2 also holds, the group is said to be an albelian group
+   */
+}
+
+#[test]
+fn test_mod_arith_ring() {
+  /*
+   * properties p1 through p10 establish that Zm is a ring.
+   */
 }
