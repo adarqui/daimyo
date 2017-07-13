f593de2cd993628a27d717b0ad5b5b8d7ee07796
Andrew Darqui <andrew.darqui@gmail.com>
2017-07-13 02:38:24 -0400

    working

--- rust/lib/math/modulo.rs
+++ rust/lib/math/modulo.rs
@@ -1,16 +1,12 @@
-use std::ops::Rem;
-
-
-
 pub trait ModuloSignedExt {
-    fn modulox(&self, n: Self) -> Self;
+    fn modulo(&self, n: Self) -> Self;
 }
 
 macro_rules! modulo_signed_ext_impl {
     ($($t:ty)*) => ($(
         impl ModuloSignedExt for $t {
             #[inline]
-            fn modulox(&self, n: Self) -> Self {
+            fn modulo(&self, n: Self) -> Self {
                 (self % n + n) % n
             }
         }
@@ -21,6 +17,7 @@ modulo_signed_ext_impl! { i8 i16 i32 i64 }
 
 
 /*
+use std::ops::Rem;
 pub fn modulo(a: i64, m: i64) -> i64 {
   // a % m
   a.rem(m)
@@ -30,8 +27,8 @@ pub fn modulo(a: i64, m: i64) -> i64 {
 
 
 pub fn a_is_congruent_to_b_modulo_m(a: i64, b: i64, m: i64) -> bool {
-  let r1 = a.modulox(m);
-  let r2 = b.modulox(m);
+  let r1 = a.modulo(m);
+  let r2 = b.modulo(m);
   r1 == r2
 }
 
@@ -39,8 +36,8 @@ pub fn a_is_congruent_to_b_modulo_m(a: i64, b: i64, m: i64) -> bool {
 
 #[test]
 fn test_modulo() {
-  assert_eq!(101.modulox(7), 3);
-  assert_eq!((-101).modulox(7), 4);
+  assert_eq!(101.modulo(7), 3);
+  assert_eq!((-101).modulo(7), 4);
   // assert_eq!(-101 % 7, -3);
 }
 
