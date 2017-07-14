90581f1f60e4e4fca9b5f2317ac0c311fce17bae
Andrew Darqui <andrew.darqui@gmail.com>
2017-07-14 05:07:09 -0400

    bang life.. helps

--- rust/lib/basic/enums.rs
+++ rust/lib/basic/enums.rs
@@ -1,4 +1,12 @@
-#[allow(dead_code)]
+// https://doc.rust-lang.org/book/first-edition/enums.html
+
+
+
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+
+
 enum Color {
   Red,
   Green,
@@ -7,9 +15,20 @@ enum Color {
 
 
 
-#[allow(dead_code)]
 enum SumType {
   Color,
   Colors(Vec<Color>),
-  Coloring{color: Color, intensity: u64}
+  Coloring{color: Color, intensity: f64}
+}
+
+
+
+enum ScopedNoProblem {
+  Color
+}
+
+
+
+fn basic_enums_sumtype() {
+  let coloring = SumType::Coloring{color: Color::Red, intensity: 77.6};
 }
--- rust/lib/basic/matching.rs
+++ rust/lib/basic/matching.rs
@@ -1,4 +1,8 @@
-#[allow(dead_code)]
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+
+
 enum Color {
   Red = 0x01,
   Green = 0x02,
@@ -7,8 +11,6 @@ enum Color {
 
 
 
-#[allow(dead_code)]
-#[allow(unused_variables)]
 fn basic_matching() {
   let v = 0;
   match v {
