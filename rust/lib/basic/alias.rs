c0573b0fa6a5be6e3657219086112addba68dae2
Andrew Darqui <andrew.darqui@gmail.com>
2017-07-14 23:31:06 -0400

    alias

--- rust/lib/basic/alias.rs
+++ rust/lib/basic/alias.rs
@@ -1,4 +1,25 @@
+// https://doc.rust-lang.org/book/first-edition/type-aliases.html
+
+
+
+use std::result;
+
+
+
 type CamelCase = u8;
 
 
+
 type snake_case = u8;
+
+
+
+type U8Result<T> = result::Result<T, u8>;
+type Result<T> = result::Result<T, u8>;
+
+
+
+fn basic_aliases() {
+  let x: CamelCase = 0;
+  let y: snake_case = 1;
+}
