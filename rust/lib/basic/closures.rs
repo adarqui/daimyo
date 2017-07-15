123b0f8c8ed4d011539e5a2c2ba4a9b8069c5041
Andrew Darqui <andrew.darqui@gmail.com>
2017-07-14 23:10:18 -0400

    blehele

--- rust/lib/basic/closures.rs
+++ rust/lib/basic/closures.rs
@@ -1,3 +1,7 @@
+// https://doc.rust-lang.org/book/first-edition/closures.html
+
+
+
 fn basic_closures() {
   fn cb_fn (i: u32) -> u32 { i + 1 };
   let cb_closure = |i| i + 1;
@@ -11,7 +15,69 @@ fn basic_closures() {
 
   let mutable_closure = |mut i| i += 1;
   mutable_closure(n);
+}
 
+
+
+
+fn basic_closures_illegal() {
+  // illegal
+  let mut num = 5;
+  let plus_num = |x: i32| x + num;
+
+  // let y = &mut num;
+
+  // legal
+  let mut num = 5;
+  { let plus_num = |x: i32| x + num; }
+  let y = &mut num;
+}
+
+
+
+
+fn basic_closures_move() {
+  let mut num = 5;
+  {
+    // mutable refernence to num
+    let mut add_num = |x| num += x;
+    add_num(5)
+  }
+  let _ = num == 6;
+
+  {
+    // ownership copy of num
+    // gets its own stack
+    let mut add_num = move |x| num += x;
+    add_num(5);
+  }
+  let _ = num == 5;
+}
+
+
+
+// function pointer
+fn nop() {
+}
+
+fn basic_function_pointer() {
+  let f = nop;
+          // call: defined below
+  let _ = call(&f);
+}
+
+
+
+// returning closures
+fn factory() -> Box<Fn(bool) -> bool> {
+  // error: |x| !x
+  // error: |x: bool| -> bool { !x }
+  Box::new(move |x| x)
+}
+
+fn basic_returning_closures() {
+  let f = factory();
+  let _ = f(true) == false;
 }
 
 
@@ -31,21 +97,41 @@ fn call_example() {
 
 
 
-fn apply<F>(f: F) where F: FnOnce() {
+// trait system overloads operators
+// three separate traits to overload with: Fn, FnMut, FnOnce
+// || {} is syntax for closures is sugar for these three traits ^^
+
+
+
+// static dispatch with Fn* traits
+fn static_apply<F>(f: F) where F: FnOnce() {
   f()
 }
 
-fn apply_op<F>(n: i32, f: F) -> i32 where F: Fn(i32) -> i32 {
+fn static_apply_op<F>(n: i32, f: F) -> i32 where F: Fn(i32) -> i32 {
   f(n)
 }
 
-fn apply_mut_op<F>(mut f: F) where F: FnMut() {
+fn static_apply_mut_op<F>(mut f: F) where F: FnMut() {
   f()
 }
 
-fn apply_example() {
+fn static_apply_example() {
   let mut x = 0;
-  apply(||x+=1);
-  apply_op(x, |i| i*2);
-  apply_mut_op(|| x=10);
+  static_apply(||x+=1);
+  static_apply_op(x, |i| i*2);
+  static_apply_mut_op(|| x=10);
+}
+
+
+
+
+// dynamic dispatch with Fn* traits
+fn dynamic_apply_op(n: i32, f: &Fn(i32) -> i32) -> i32 {
+  f(n)
+}
+
+fn dynamic_apply_example() {
+  let mut x = 0;
+  dynamic_apply_op(x, &|i| i*2);
 }
