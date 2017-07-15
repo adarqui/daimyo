99c1d31b8fbc9f1f2a0438a46ee5c16c86a5316b
Andrew Darqui <andrew.darqui@gmail.com>
2017-07-14 23:33:02 -0400

    alias

--- rust/lib/basic/casting.rs
+++ rust/lib/basic/casting.rs
@@ -1,7 +1,20 @@
+// https://doc.rust-lang.org/book/first-edition/casting-between-types.html
+
+
+
+// safe casts: as
+// dangerous casts: transmute
+
+
+
 fn basic_casting() {
   let n32 = 12345678u32;
   let n64 = n32 as u64;
 
   let a_hex = 0x41u64;
   let a_char = a_hex as u8;
+
+  // most common case of casting, removing mutability from a ref
+  let mut q: &bool = true;
+  let r = &q as q;
 }
