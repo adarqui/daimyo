f531cc7abb635624159dd9e8fc1431f21ad75fd6
Andrew Darqui <andrew.darqui@gmail.com>
2017-07-14 23:24:54 -0400

    more const

--- rust/lib/basic/constants.rs
+++ rust/lib/basic/constants.rs
@@ -1,5 +1,22 @@
+// static: not inlined.
 static LANGUAGE: &'static str = "rust";
+static mut COUNTER: u32 = 0;
 
 
 
+fn basic_mut_constants() {
+  let current_counter: u32;
+
+  unsafe {
+    COUNTER += 1;
+  }
+
+  unsafe {
+    current_counter = COUNTER;
+  }
+}
+
+
+
+// const: inlined, doesn't occupy memory
 const SOME_CONSTANT: bool = true;
