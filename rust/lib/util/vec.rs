681328217b673acc5efbdbf7caf10bd8356f84c6
Andrew Darqui <andrew.darqui@gmail.com>
2017-07-20 21:46:58 -0400

    okdfo

--- rust/lib/util/vec.rs
+++ rust/lib/util/vec.rs
@@ -52,3 +52,36 @@ pub fn string_to_vec_of_i64_m26(s: &str) -> Vec<i64> {
 fn test_string_to_vec_of_i64_m26() {
   assert_eq!(string_to_vec_of_i64_m26("hello"), vec![07, 04, 11, 11, 14]);
 }
+
+
+
+/// convert a Vec<i64> to a String
+///
+pub fn vec_of_i64_to_string(v: Vec<i64>) -> String {
+  let mut s = String::new();
+
+  for i in v {
+    s.push(char::from_u32(i as u32).unwrap());
+  }
+
+  s
+}
+
+#[test]
+fn test_vec_of_i64_to_string() {
+  assert_eq!(vec_of_i64_to_string(vec![104, 101, 108, 108, 111]), "hello");
+}
+
+
+
+/// convert a Vec<i64> mod 26 to a String
+///
+pub fn vec_of_i64_m26_to_string(v: Vec<i64>) -> String {
+  let v_: Vec<i64> = v.into_iter().map(|c| c + 97).collect();
+  vec_of_i64_to_string(v_)
+}
+
+#[test]
+fn test_vec_of_i64_m64_to_string() {
+  assert_eq!(vec_of_i64_m26_to_string(vec![07, 04, 11, 11, 14]), "hello");
+}
