f812b4546fcc3df779778f2161f6ba7d24c2ea05
Andrew Darqui <andrew.darqui@gmail.com>
2017-07-23 07:59:18 -0400

    moving tests out

--- rust/lib/util/tests/vec.rs
+++ rust/lib/util/tests/vec.rs
@@ -1,29 +1,15 @@
+#[cfg(test)]
+
+extern crate util;
 use std::char;
-use char as util_char;
+use util::char as util_char;
+use util::vec::*;
 
 
 
-/// convert a string to a Vec<char>
-///
-pub fn string_to_vec_of_char(s: &str) -> Vec<char> {
-  s
-  .as_bytes()
-  .into_iter()
-  .map(|x| char::from_u32(*x as u32)
-  .unwrap())
-  .collect()
-}
-
-
-
-/// convert a string to a Vec<i64>
-///
-pub fn string_to_vec_of_i64(s: &str) -> Vec<i64> {
-  s
-  .as_bytes()
-  .into_iter()
-  .map(|x| x.clone() as i64)
-  .collect()
+#[test]
+fn test_string_to_vec_of_i64() {
+  assert_eq!(string_to_vec_of_i64("hello"), vec![104, 101, 108, 108, 111]);
 }
 
 
@@ -36,6 +22,11 @@ pub fn string_to_vec_of_i64_m26(s: &str) -> Vec<i64> {
   .into_iter()
   .map(|x| (util_char::char_to_base(char::from_u32(*x as u32).unwrap())).clone() as i64)
   .collect()
+}
+
+#[test]
+fn test_string_to_vec_of_i64_m26() {
+  assert_eq!(string_to_vec_of_i64_m26("hello"), vec![07, 04, 11, 11, 14]);
 }
 
 
@@ -52,6 +43,11 @@ pub fn vec_of_i64_to_string(v: Vec<i64>) -> String {
   s
 }
 
+#[test]
+fn test_vec_of_i64_to_string() {
+  assert_eq!(vec_of_i64_to_string(vec![104, 101, 108, 108, 111]), "hello");
+}
+
 
 
 /// convert a Vec<i64> mod 26 to a String
@@ -59,4 +55,9 @@ pub fn vec_of_i64_to_string(v: Vec<i64>) -> String {
 pub fn vec_of_i64_m26_to_string(v: Vec<i64>) -> String {
   let v_: Vec<i64> = v.into_iter().map(|c| c + 97).collect();
   vec_of_i64_to_string(v_)
+}
+
+#[test]
+fn test_vec_of_i64_m64_to_string() {
+  assert_eq!(vec_of_i64_m26_to_string(vec![07, 04, 11, 11, 14]), "hello");
 }
